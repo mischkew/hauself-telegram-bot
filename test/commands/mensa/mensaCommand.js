a20ca9f7aff02d28ea67be7779de80b004f84608
Sven Mkw <sven.mkw@gmail.com>
2016-05-29 15:01:48 +0200

    removed deprecated imports

--- test/commands/mensa/mensaCommand.js
+++ test/commands/mensa/mensaCommand.js
@@ -1,14 +1,5 @@
 import { expect } from 'chai'
 import debug from 'debug'
-import moment from 'moment'
-import { today,
-         tomorrow,
-         nextMonday,
-         isWeekend,
-         hours,
-         nextMealDate,
-         menuToMarkdown
-       } from '../../../src/commands/mensa/mensaCommand'
 const log = debug('test')
 
 describe('Mensa Command', () => {
