f3a30320788d55350ae6c550120056028f28c79d
Sven Mkw <sven.mkw@gmail.com>
2016-08-15 14:04:29 +0200

    use strict mode always

--- bootstrap.js
+++ bootstrap.js
@@ -12,18 +12,26 @@ require('source-map-support').install()
 // Enable ES6
 //
 
+// have every file in strict mode
+require("babel-core").transform("code", {
+  plugins: ["transform-strict-mode"]
+})
+
 // node_modules written in es6 which have to be compiled as well.
 // Add packges names here.
 var ES6_PACKAGES = [
   'fetch-mock',
   'fetch-api',
-  'telegraf'
+  'telegraf',
+  'telegraf-flow',
+  'telegraf-session-redis'
 ]
 require('babel-core/register')({
   // ignore 3rd party packages
   // but build those which are written in es6 purely
   ignore: new RegExp('/(?!node_modules\/(' + ES6_PACKAGES.join('|') + '))node_modules/')
 })
+
 
 //
 // Execute Polyfills
